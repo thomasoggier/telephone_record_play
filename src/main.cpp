0b9a169f409bd25b0e9c77abedcc562c136568a6
Thomas Oggier <thomas.oggier@soprod.ch>
2023-04-17 09:43:01 +0200

    add submodule Adafruit_VS1053_Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
-void setup() {
+#include <SPI.h>
+#include <Adafruit_VS1053.h>
+#include <SD.h>
+
+void setup()
+{
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Adafruit VS1053 Simple Test");
+  Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(RESET, CS, DCS, DREQ, CARDCS);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  Serial.println(".");
+  delay(1000);
 }
